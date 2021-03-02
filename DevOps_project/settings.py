d084c2d4bc792bb9921a4d5e0e2e953a935ed0c0
mouadelhaddad <58404501+mouadelhaddad@users.noreply.github.com>
2021-03-02 13:07:31 +0100

    Update settings.py

--- DevOps_project/settings.py
+++ DevOps_project/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'DevOps_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgres',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
